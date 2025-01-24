3452f9341171f4a897316fe17ef9a8d3f6d24251
antoo69 <antobarry69@gmail.com>
2025-01-24 20:31:17 +0700

    Update MessageHandler.py

--- handlers/users/MessageHandler.py
+++ handlers/users/MessageHandler.py
@@ -1,4 +1,3 @@
-
 import asyncio
 
 from aiogram import types
@@ -15,14 +14,12 @@ from utils.FontsChanger import fonts_number
 from data.Users import specific_font, all_fonts
 from utils.db_api.bot_users import create_bot_user
 
-
 @dp.message_handler(text="📋 Fonts list 📝")
 async def menu_handler1(message: types.Message):
     txt = page_generator(1, 10)
 
     buttons = await generate_buttons(1, 10)
     await message.answer(text=txt, reply_markup=buttons)
-
 
 @dp.message_handler(text="☑️ Apply all fonts ✅")
 async def menu_handler2(message: types.Message):
@@ -31,7 +28,6 @@ async def menu_handler2(message: types.Message):
     txt = "<b>Send me some text 🖋</b>"
 
     await message.answer(text=txt)
-
 
 @dp.message_handler(text="🤖 About Inline Mode 📃")
 async def aboutInlineMode(message: types.Message):
@@ -42,14 +38,13 @@ async def aboutInlineMode(message: types.Message):
             message_id=9
         )
     except:
-        print("Inline mode haqida video forward qilishda xatolik!")
-
+        print("Error while forwarding the Inline Mode video!")
 
 # Echo bot
 @dp.message_handler()
 async def bot_echo(message: types.Message):
 
-    # User'ni database'ga saqlaymiz
+    # Save the user in the database
     create_bot_user(message.from_user)
 
     user_id = message.from_user.id
@@ -57,39 +52,37 @@ async def bot_echo(message: types.Message):
     temp1 = all_fonts.get(user_id)
     temp2 = specific_font.get(user_id)
 
-    # admin uchun xabar
-    xabar = f"#U{message.from_user.id}\n\n"
-    xabar += f"<b>Name</b>:   {message.from_user.full_name}\n"
-    xabar += f"<b>ID:</b>    {message.from_user.id}\n"
-    xabar += f"<b>Username:</b>   @{message.from_user.username}\n"
-    xabar += f"<b>Lang Code:</b>   #{message.from_user.language_code}\n\n"
+    # Message for admin
+    admin_message = f"#U{message.from_user.id}\n\n"
+    admin_message += f"<b>Name</b>:   {message.from_user.full_name}\n"
+    admin_message += f"<b>ID:</b>    {message.from_user.id}\n"
+    admin_message += f"<b>Username:</b>   @{message.from_user.username}\n"
+    admin_message += f"<b>Lang Code:</b>   #{message.from_user.language_code}\n\n"
 
     if temp1 is True:
         for i in range(fonts_number):
             await message.answer(font_changer(message.text, i))
             await asyncio.sleep(0.05)
         
-        # adminga xabar beramiz
+        # Notify admin
         try:
-            xabar += message.text
-            await dp.bot.send_message(text=xabar, chat_id=1039835085)
+            admin_message += message.text
+            await dp.bot.send_message(text=admin_message, chat_id=1039835085)
         except Exception as e:
-            print("Adminga xabar berishda xatolik")
+            print("Error while sending a message to admin")
     
     elif temp2:
         await message.answer(font_changer(message.text, temp2))
 
-        # adminga xabar beramiz
+        # Notify admin
         try:
-            xabar += font_changer(message.text, temp2)
-            await dp.bot.send_message(text=xabar, chat_id=1039835085)
+            admin_message += font_changer(message.text, temp2)
+            await dp.bot.send_message(text=admin_message, chat_id=1039835085)
         except:
-            print("Adminga xabar berishda xatolik")
+            print("Error while sending a message to admin")
     
     else:
         txt = "<i>Please, select a font from the</i> <b>📋 Fonts list 📝 </b> <i>section, \
-        \nor click</i> <b>☑️ Apply all fonts ✅</b><i> button to use all fonts at once</i>"
+        or click</i> <b>☑️ Apply all fonts ✅</b><i> button to use all fonts at once</i>"
 
         await message.answer(text=txt, reply_markup=menu_button)
-
-    
