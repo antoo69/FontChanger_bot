4845f479733e34f00ee39a19d520ca0987434749
antoo69 <antobarry69@gmail.com>
2025-01-24 20:42:55 +0700

    Update notify_admins.py

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -8,8 +8,7 @@ from data.config import ADMINS
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, "Bot ishga tushdi")
-
+            await dp.bot.send_message(admin, "The bot has started")
         except Exception as err:
             pass
             # logging.exception(err)
