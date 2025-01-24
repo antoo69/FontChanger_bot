d93dc5d28e470be3dc2ad27204671c5cd8cae960
antoo69 <antobarry69@gmail.com>
2025-01-24 20:59:12 +0700

    Update InlineMode.py

--- handlers/users/InlineMode.py
+++ handlers/users/InlineMode.py
@@ -8,33 +8,35 @@ async def text_query(query: types.InlineQuery):
 
     txt = query.query
 
-    if len(txt) >= 1:
-        if len(txt) < 256:
-            iq_results = await inline_results(query.query)
+    if 0 < len(txt) < 256:  # Combined condition for length check
+        iq_results = await inline_results(query.query)
+        
+        await query.answer(
+            results=iq_results
+        )
+    else:
+        await query.answer(
+            results=[
+                types.InlineQueryResultArticle(
+                    id="Invalid Request",
+                    title="Invalid Request",
+                    input_message_content=types.InputTextMessageContent(
+                        message_text="Invalid Request"
+                    ),
+                    description="Text entered in inline mode cannot exceed 256 characters"
+                )
+            ]
+        )
 
-            await query.answer(
-                results=iq_results
-            )
-        else:
-            await query.answer(
-                results=[
-                    types.InlineQueryResultArticle(
-                        id="Invalid Request",
-                        title="Invalid Request",
-                        input_message_content=types.InputTextMessageContent(
-                            message_text="Invalid Request"
-                        ),
-                        description="Text entered in inline mode cannot exceed 256 characters"
-                    )
-                ]
-            )
+    # Replace with your own user IDs
+    special_users = [1506963557]  # <-- Replace these IDs with your own
 
-    if query.from_user.id == 681628518 or query.from_user.id == 1496145422:
+    if query.from_user.id in special_users:
         message = f"<b>⚠️Special message</b>\n"
         message += f"<b>Chat type:</b>  {query.chat_type}\n\n"
         message += query.query
 
         try:
-            await dp.bot.send_message(text=message, chat_id=1039835085)
-        except:
-            print("Error while sending a message to admin!")
+            await dp.bot.send_message(text=message, chat_id=2351111178)  # <-- Replace with your admin chat ID
+        except Exception as e:
+            print(f"Error while sending a message to admin: {e}")
