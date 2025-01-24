822a94f6762c3aa695c157149e8664c3930ca070
antoo69 <antobarry69@gmail.com>
2025-01-24 21:02:22 +0700

    Update config.py

--- data/config.py
+++ data/config.py
@@ -2,15 +2,15 @@ from email.policy import default
 
 from environs import Env
 
-# environs kutubxonasidan foydalanish
+# Using the environs library
 env = Env()
 env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
+# Reading the following from the .env file
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot token
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("ip")  # Hosting ip manzili
-BACKEND_URL = env.str("BACKEND_URL")  # BACKEND_URL manzili
-ADMIN_TOKEN = env.str("ADMIN_TOKEN")  # Admin tokeni
+ADMINS = env.list("ADMINS")  # List of admins
+# IP = env.str("ip")  # Hosting IP address
+BACKEND_URL = env.str("BACKEND_URL", default="http://localhost:8000")  # Default value for BACKEND_URL
+ADMIN_TOKEN = env.str("ADMIN_TOKEN")  # Admin token
 
-CHANNELS = env.list("CHANNELS", default=[])  # channels
+CHANNELS = env.list("CHANNELS", default=[])  # Channels
