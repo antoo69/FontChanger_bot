c68a8261dc6ca7489b7c8a2db0e2608f97561014
antoo69 <antobarry69@gmail.com>
2025-03-13 19:00:16 +0700

    Update checksub.py

--- middlewares/checksub.py
+++ middlewares/checksub.py
@@ -2,9 +2,6 @@ import logging
 from aiogram import types
 from aiogram.dispatcher.handler import CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
-
-from data.config import CHANNELS
-from utils.misc.subscription import check_subscription
 from loader import bot
 
 
@@ -18,20 +15,8 @@ class BigBrother(BaseMiddleware):
             user = update.callback_query.from_user.id
         else:
             return
-        logging.info(user)
+        
+        logging.info(f"User ID: {user}")  # Log hanya ID user tanpa cek fsub
 
-        result = "Please, join below channel to use this bot\n\n"
-        final_status = True
-
-        for channel in CHANNELS:
-            status = await check_subscription(user_id=user, channel=channel)
-            final_status *= status
-            channel = await bot.get_chat(channel)
-
-            if not status:
-                invite_link = await channel.export_invite_link()
-                result += f"   👉 <a href='{invite_link}'> {channel.title} </a>\n"
-
-        if not final_status:
-            await update.message.answer(result, disable_web_page_preview=True)
-            raise CancelHandler()
+        # Tidak ada forced sub, langsung lanjut
+        return  
